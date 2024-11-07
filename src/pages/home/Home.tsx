2ca3ae16e7918ce6e96b77411ec037b804f75233
Ronney Rocha <ronneyrv@gmail.com>
2024-11-07 14:44:21 -0300

    refactor: update code home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,15 @@
 import NavBar from "../../components/NavBar";
+import About from "./sections/About/About";
 import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
 
 function Home() {
   return (
     <>
       <NavBar />
       <Hero />
+      <About />
+      <Projects />
     </>
   );
 }
