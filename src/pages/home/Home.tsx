8ceaecc2c79e3dd69c370e2b1668d975bddfb290
Ronney Rocha <ronneyrv@gmail.com>
2024-11-07 09:15:09 -0300

    feat: creat componet Home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar";
+import Hero from "./sections/Hero/Hero";
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  );
+}
+
+export default Home;
